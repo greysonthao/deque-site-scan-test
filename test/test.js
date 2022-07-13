3fe5f73779d8236ae7f65abd48eec12ac4ff341b
Touger Thao <tougerthao@gmail.com>
2022-07-13 17:08:34 -0500

    finished scan

---
+++ test/test.js
@@ -0,0 +1,36 @@
+const { By, Key, Builder, Capabilities } = require("selenium-webdriver");
+const chromeCapabilities = Capabilities.chrome();
+require("chromedriver");
+const should = require("chai").should();
+const AxeBuilder = require("@axe-core/webdriverjs");
+chromeCapabilities.set("chromeOptions", { args: ["--headless"] });
+
+describe("deque challenge tests", function () {
+  it("main-nav loads successfully", async function () {
+    const driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get("https://dequeuniversity.com/demo/mars");
+
+    const mainNavIsDisplayed = await driver
+      .findElement(By.id("main-nav"))
+      .isDisplayed();
+
+    mainNavIsDisplayed.should.equal(true);
+
+    driver.quit();
+  });
+  it("Axebuilder successfully analyzes page", async function () {
+    const driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get("https://dequeuniversity.com/demo/mars/").then(() => {
+      new AxeBuilder(driver).analyze((err, results) => {
+        if (("Error: ", err)) {
+          console.log("Errors: ", err);
+        }
+        console.log("Results: ", results);
+      });
+
+      driver.quit();
+    });
+  });
+});
